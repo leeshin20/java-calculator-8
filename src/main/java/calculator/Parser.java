8fc29be9992af681f62054747064fb8de9ac1871
leeshin20 <leeshin20@naver.com>
2025-10-20 20:09:23 +0900

    feat: 숫자가 음수인 경우 예외처리 추가

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -11,8 +11,15 @@ public class Parser {
 
         for (String stringNum : stringNumbers) {
             int number = Integer.parseInt(stringNum.trim());
+            checkNumberIsPositive(number);
             numbers.add(number);
         }
         return numbers;
+    }
+
+    private static void checkNumberIsPositive(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수가 입력되었습니다. 양수만 입력해주세요.");
+        }
     }
 }
